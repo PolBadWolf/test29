1a9676e9df33450249fad3c96c6928c5508c8ac4
PolBadWolf <gm.gameoff@gmail.com>
2020-07-29 14:41:47 +0500

    test to array

--- src/main/java/org/example/test29/MainClass.java
+++ src/main/java/org/example/test29/MainClass.java
@@ -1,6 +1,7 @@
 package org.example.test29;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class MainClass {
 
@@ -12,12 +13,19 @@ public class MainClass {
         System.out.println("start programm");
 
         try {
-            String instanceName = "127.0.0.1\\SQLEX";
+//            String instanceName = "127.0.0.1\\SQLEX";
+            String instanceName = "127.0.0.1\\SQLEX:1433";
             String databaseName = "spc1";
             String userName = "max";
             String pass = "1122";
             String connectionUrl = "jdbc:sqlserver://%1$s;databaseName=%2$s;user=%3$s;password=%4$s;";
             String connectionString = String.format(connectionUrl, instanceName, databaseName, userName, pass);
+
+            try {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
 
             Connection connection = DriverManager.getConnection(connectionString);
 
@@ -32,8 +40,24 @@ public class MainClass {
                 System.out.println(id_spec + "\t" + ves);
             }
 
+            executeQuery.close();
+            stmt.close();
+
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO Table_1(id_spec, ves, dis) VALUES (?, ?, ?)");
+            statement.setInt(1, 2);
+            statement.setInt(2, 223);
+
+            ArrayList<Ddt> tt = new ArrayList<>();
+            for (int i = 16_380; i < 16_384; i++) {
+                tt.add(new Ddt(i, i));
+            }
+
+            byte[] q = Ddt.toBytes( tt.toArray(new Ddt[tt.size()]));
+            q = null;
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
 }
